b11f6aaf1ebc6b65622462cb2fe5a043f27f62ff
DESKTOP-5K41LSK <mahnoor2809@gmail.com>
2019-11-18 11:51:09 +0500

    Python Code

---
+++ ques3.py
@@ -0,0 +1,16 @@
+print(" \t My Library ")
+print("1 Fiction Books \n2 Non_Fiction Books ")
+fiction = 1
+nonfiction = 2
+books = int(input("Type of Book  : "))
+if(books == 1):
+    print("A Comedy \nB Comic/Graphic_Novel \nC Science_Fiction \nD Fantasy \nE Historical_Fiction ")
+elif(books == 2):
+    print("F History&Geography \nG Arts \nH Science&Technolgy \nI Others")
+else:
+    print("This type of book is not available ")
+
+
+
+
+
