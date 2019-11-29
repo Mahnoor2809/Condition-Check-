7180876feafb84d3e62ae91c37cbbf49c12e6dbe
Mahnoor Hafeez <50525665+Mahnoor2809@users.noreply.github.com>
2019-11-29 19:44:20 +0500

    Update ques3.py

--- ques3.py
+++ ques3.py
@@ -1,14 +1,14 @@
 print(" \t My Library ")
-print("1 Fiction Books \n2 Non_Fiction Books ")
+print(" 1 Fiction Books \n2 Non_Fiction Books ")
 fiction = 1
 nonfiction = 2
-books = int(input("Type of Book  : "))
+books = int(input(" Type of Book  : "))
 if(books == 1):
-    print("A Comedy \nB Comic/Graphic_Novel \nC Science_Fiction \nD Fantasy \nE Historical_Fiction ")
+    print(" A Comedy \nB Comic/Graphic_Novel \nC Science_Fiction \nD Fantasy \nE Historical_Fiction ")
 elif(books == 2):
-    print("F History&Geography \nG Arts \nH Science&Technolgy \nI Others")
+    print(" F History&Geography \nG Arts \nH Science&Technolgy \nI Others")
 else:
-    print("This type of book is not available ")
+    print(" This type of book is not available ")
 
 
 
